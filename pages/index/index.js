cf4db6dad1288dce9ac40bba997de1dde55417da
zhouchuang <zhouchuang1217@163.com>
2019-06-15 17:03:38 +0800

    frist commit

---
+++ pages/index/index.js
@@ -0,0 +1,114 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    type:'primary',
+    count: 0,
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    disabled: false,
+    plain: false,
+    loading: false,
+    times: 0,
+    min: 0,
+    status: '开始'
+  },
+  timer: 0,
+  breastfeed: { list: [] },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  clickHander:function(){
+    if (this.data.type =='primary'){
+      this.start();
+    }else {
+      this.end();
+    }
+  },
+  start: function(){
+    this.setData({
+      status:'结束',
+      type:'warn'
+    });
+    var num = 0;
+    this.timer = setInterval(() => {
+      this.setData({
+        times: ++num,
+        min: Math.floor(num / 60)
+      });
+    }, 1000);
+    this.breastfeed = wx.getStorageSync('breastfeed') || { list: [] };
+    var date = { date: util.formatTime(new Date()), times: 1 };
+    this.breastfeed.list.push(date);
+  },
+  end: function () {
+    clearInterval(this.timer);
+    this.breastfeed.list[this.breastfeed.list.length - 1]['times'] = this.data.times;
+    this.setData({
+      list: this.breastfeed.list,
+      times: 0,
+      min: 0,
+      status: '开始',
+      type: 'primary'
+    });
+    wx.setStorage({
+      key: 'breastfeed',
+      data: this.breastfeed,
+    });
+    this.history();
+  },
+  history:function(){
+    wx.navigateTo({
+      url: '../main/main'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    // var breastfeed = wx.getStorageSync('breastfeed')||{count:0};
+    // this.setData({
+    //   count:breastfeed.count
+    // });
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
---
+++ pages/main/main.js
@@ -0,0 +1,90 @@
+const util = require('../../utils/util.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    times:0,
+    min:0,
+    list: []
+  },
+  timer:0,
+  breastfeed:{list:[]},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    this.breastfeed = wx.getStorageSync('breastfeed') || { list: [] };
+    this.transfer();
+  },
+
+  transfer:function(){
+    if (this.breastfeed.list && this.breastfeed.list.length) {
+      var temp = [];
+      for (var i in this.breastfeed.list) {
+        temp.push({
+          min: Math.ceil(this.breastfeed.list[i].times / 60),
+          times: this.breastfeed.list[i].times,
+          per: Math.ceil(this.breastfeed.list[i].times/20),
+          date: this.breastfeed.list[i].date
+        });
+      }
+      this.setData({
+        list: temp
+      });
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
